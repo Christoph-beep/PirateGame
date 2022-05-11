20d25c428170afd702d1041372288b936d530b48
Christoph <31371749+Christoph-beep@users.noreply.github.com>
2022-05-11 16:18:43 +0200

    Add files via upload
    - Fixing von unperformanten Code
    - Möglicheit, deine eigenen Ideen von Beute hinzuzufügen

--- Main.java
+++ Main.java
@@ -7,15 +7,21 @@ public class Main {
 
     private static int beuteCaptain;
     private static int beuteCrew; // Das erhält die gesamte Crew
-    public static int beuteJeMitglied; // Anteil, den jedes Mitglied erhält
-    protected static String beuteartZufallsBeute;
+    private static int beuteJeMitglied; // Anteil, den jedes Mitglied erhält
+    private static String beuteartZufallsBeute;
     private static int beute;
-    protected static int banditenAnzahl;
-    private static int spielBeenden = 0;
-    private static ArrayList beuteArten = new ArrayList<String>();
+    private static int banditenAnzahl;
+
+    private static int spielAuswahl = 1;
+    public static ArrayList beuteArten = new ArrayList<String>();
+
     // Wenn spielBeenden 1 ist, wird das Spiel beendet
 
     public static void main(String[] args) {
+        // array wird initialisiert
+        beuteArten.add(" Kisten Gold");
+        beuteArten.add(" Goldstücke");
+        beuteArten.add(" Schnapsflaschen");
     spielen();
 
 
@@ -25,7 +31,7 @@ public class Main {
 
         // Hier wird unser Scanner definiert
         Scanner scan = new Scanner(System.in);
-        while (spielBeenden != 1) {
+        while (spielAuswahl !=0) {
             // Die BeuteArt wird jede Rund neu ermittelt
             beuteErmitteln();
             System.out.println("Bitte gib die Anzahl der Personen an, die sich inklusive dir auf auf den Beutezug begeben , grundsätzlich bekommt der Captain die Hälfte der Beute, der Rest wird gleichmässig aufgeteilt ");
@@ -33,27 +39,30 @@ public class Main {
             try {
 
                 banditenAnzahl = scan.nextInt();
-
                 double beuteMengeUngerundet;
                 // Hier wird die Anzahl der Beute ermittelt
                 beuteMengeUngerundet = Math.random() * 100 + 100;
                 beute = (int) Math.round(beuteMengeUngerundet);
+                System.out.println(beute);
                 // Anzahl Beute je Mitglied
-                beuteJeMitglied = beute / 2 / banditenAnzahl;
-                beuteJeMitglied = (int) Math.ceil(beuteJeMitglied);
+                beuteJeMitglied = (int) Math.ceil(beute / 2.0 / banditenAnzahl);
+                System.out.println(beuteJeMitglied);
                 // Alles, was die gesamte crew erhält
                 beuteCrew = beuteJeMitglied * banditenAnzahl;
 
                 // Berechnung Gold für die Crew
-                System.out.println("Jedes Crewmitglied erhält " + getBeuteJeMitglied() + beuteartZufallsBeute + " erhalten");
+                System.out.println("Jedes Crewmitglied erhält " + getBeuteJeMitglied() + getBeuteartZufallsBeute() );
                 // Berechnung Gold Captain
                 beuteCaptain = beute - beuteCrew;
-                System.out.println("Unser Captain hat " + getBeuteCaptain() + beuteartZufallsBeute + " erhalten");
+                System.out.println("Unser Captain hat " + getBeuteCaptain() + getBeuteartZufallsBeute() );
 
 
                 // Erneute Spielabfrage
-                System.out.println("Wollt ihr erneut auf Beutezug gehen ? 0 steht für Ja und 1 für nein");
-                spielBeenden = scan.nextInt();
+                System.out.println("Wollt ihr erneut auf Beutezug gehen, das Spiel beenden oder einen neuen erbeutbaren Gegenstand hinzufügen ? Fürs spielen bitte die 0 wählen, zum spielen die 1 und zum hinzufügen von Beute die 2");
+                spielAuswahl= scan.nextInt();
+                if (spielAuswahl == 2){
+                    beuteHinzufügen();
+                }
             } catch (Exception InputMismatchException){
                 System.out.println("Bitte gib eine Zahl an");
                 spielen();
@@ -73,41 +82,50 @@ public class Main {
     }
 
 
-    public static int getBanditenAnzahl() {
-        return banditenAnzahl;
-    }
-
-    public static void setBanditenAnzahl(int banditenAnzahl) {
-        Main.banditenAnzahl = banditenAnzahl;
-    }
-
-
     public static ArrayList getBeuteArten() {
         return beuteArten;
     }
 
-    public static void setBeuteArten(ArrayList beuteArten) {
-        Main.beuteArten = beuteArten;
-    }
 
     public static int getBeuteJeMitglied() {
         return beuteJeMitglied;
     }
 
 
+    public static String getBeuteartZufallsBeute() {
+        return beuteartZufallsBeute;
+    }
+
     protected static void beuteErmitteln(){
-        double beuteartZufallsZahl;
+        int beuteartZufallsZahl;
 
         // Hier wird die Art der Beute ermittelt
-        beuteArten.add(" Kisten Gold");
-        beuteArten.add(" Goldstücke");
-        beuteArten.add(" Schnapsflaschen");
-        beuteartZufallsZahl = Math.random()*3;
+        beuteartZufallsZahl = (int) (Math.random()* beuteArten.size());
         beuteartZufallsZahl = Math.round(beuteartZufallsZahl) -1;
         beuteartZufallsBeute = String.valueOf(beuteArten.get((int) beuteartZufallsZahl));
         System.out.println(beuteartZufallsZahl);
 
 
     }
+
+    private static void beuteHinzufügen(){
+      String neueBeute;
+
+        Scanner scan = new Scanner(System.in);
+      while (spielAuswahl == 2)
+        try {
+            System.out.println("Hier kannst du deine eigenen Ideen für Beute, welche du mit Captain Ciao erbeuten kannst hinzufügen");
+            neueBeute = scan.next();
+            beuteArten.add(neueBeute);
+            beuteErmitteln();
+            System.out.println("Um einen weiteren Gegenstand hinzuzufügen bitte erneut die 2 wählen, ansonsten wieder die 1 zum weiterspielen und die 0 zum beenden");
+            spielAuswahl = scan.nextInt();
+        }  catch (Exception InputMismatchException){
+            System.out.println("Bitte gib eine gültige Zahl ein");
+        }
+      scan.close();
+
+    }
+
 
 }
